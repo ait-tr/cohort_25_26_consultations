eab759ef0febcc32c0a4e85454c0c1c5aea4f121
Alisher Khamidov <a.a.khamidov@yandex.ru>
2023-03-21 16:58:52 +0100

    consultation 2023_03_21

--- consultations/2023-03-21/code/example/src/Main.java
+++ consultations/2023-03-21/code/example/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String youngestChild;       // declare    - объявили переменную - тип и имя
+        youngestChild = "Ivan";     // initialize - присвоили первичное значение
+        youngestChild = "Simon";    // перезаписываем значение на другое
+//        System.out.println(youngestChild); // Simon
+        youngestChild = "Katya";
+//        System.out.println(youngestChild); // Katya
+        // String youngestChild = "Ira"; - нельзя заново объявить существующую переменную
+        int a = 10;
+        int b = 12;
+        int c = a + b;
+        int sum = a + c;
+        //
+        String example = "I am example";
+        System.out.println(example.charAt(2)); // a
+        System.out.println(example.length());  // 12
+        // то есть индекс последнего символа = длинна - 1
+        int index = example.length() - 1;
+        char lastSymbol = example.charAt(index);
+        System.out.println(lastSymbol);
     }
 }
